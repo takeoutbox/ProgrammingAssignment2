66e7f217c718bd4b4164fb73e577885dfb49239f
takeoutbox <bill.lyerly@gmail.com>
2015-08-23 10:56:04 -0500

    Update cachematrix.R
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-#bill
+# The makeCacheMatrix handles getting and setting the inverse matrix
+makeCacheMatrix <- function(x = matrix()){
+  
+  inver <-NULL        #initializes the variable to store the inverse matrix as null
+  
+  #creates the set function to set cached valye of X whatever was passed to y and the inver variable to null
+  set <- function (y)
+  {
+    x<<- y #caches x
+    inver<<- NULL #caches y as null
+  } 
+  
+  #creates a function to retrieve the value of x. X is not inverted
+  get <- function () X
+  setinverse <- function(inverse) inver<<- inverse # creates a function that caches the inverse 
+  getinverse <- function () inver # creates a function that retrieves the inverse
+  list (set=set, get = get, setnverse = setinverse, getinverse = getinverse) #creates a list to store the objects set, get, setinverse and getinverse
 }
 
-
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix from makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inver <- x$getinverse() #calls the getinverse and returns the value of of the cached inverse matrix
+  if (!is.null(inver)) #checks to see if inver is already cached. If it is cached returns message and variable
+  {
+    message ("getting cached data")
+    return(inver)
+  }
+  data <- x$get() #returns the uninverted variable
+  inver <- solve(data, ...) #solves for the inverted variable
+  x$setinverse(inver) #caches the inverted variable
+  inver #displays the inverted variable
 }
